4c8c2672da27889fd903c8838c55bb5d7d9561cd
erzan <do.earljan@gmail.com>
2026-03-18 19:54:30 +0800

    added page for portfolio temporarily

---
+++ apps/docs/src/pages/portfolio/index.js
@@ -0,0 +1,12 @@
+import Layout from '@theme/Layout';
+
+export default function Portfolio() {
+  return (
+    <Layout title="Portfolio">
+      <main style={{ padding: '2rem', textAlign: 'center' }}>
+        <h1>Portfolio</h1>
+        <p>🚧 Coming soon...</p>
+      </main>
+    </Layout>
+  );
+}
