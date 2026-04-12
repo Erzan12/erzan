e08c9c623a170e066d3d7825b02af4fc3e2fb01b
erzan <do.earljan@gmail.com>
2026-04-13 07:04:20 +0800

    chore(prisma-dependency): added new adapter dep for prisma

--- apps/web/src/lib/prisma/prisma.ts
+++ apps/web/src/lib/prisma/prisma.ts
@@ -1,10 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+const { PrismaPg } = require('@prisma/adapter-pg');
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({
+  new PrismaClient({ adapter, 
     log: ["query"],
   });
 
