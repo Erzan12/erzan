449abecff1d3eb3a391e53cdf3f611b91cbce801
erzan12 <do.earljan@gmail.com>
2026-05-06 16:38:37 +0800

    fix: about page can now be access without log in user sessions

--- apps/web/src/app/about/page.tsx
+++ apps/web/src/app/about/page.tsx
@@ -1,8 +1,10 @@
 import AboutClient from "@/components/core/about/about-client";
+import { authOptions } from "@/lib/auth/auth";
 import { requireAdmin } from "@/lib/route-protection/user-check";
+import { getServerSession } from "next-auth";
 
 export default async function About() {
-  const session = await requireAdmin();
+  const session = await getServerSession(authOptions); // make a page accessible to other users regardless if admin or ordinary users, ordinary users can only view posts while admin can view and create posts
   const avatar = session?.user.image;
 
   return (
