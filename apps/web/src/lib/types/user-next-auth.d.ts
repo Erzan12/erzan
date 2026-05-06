342683aa126fe9903fdb7510429e089fc4bf8744
erzan12 <do.earljan@gmail.com>
2026-05-06 14:06:04 +0800

    feat: updated user-next-auth types

--- apps/web/src/lib/types/user-next-auth.d.ts
+++ apps/web/src/lib/types/user-next-auth.d.ts
@@ -4,8 +4,16 @@ declare module "next-auth" {
   interface Session {
     user: {
       id: string;
-      email?: string | null;
       name?: string | null;
+      email?: string | null;
+      image?: string | null;
     };
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id?: string;
+    image?: string | null;
   }
 }
