85b952853a8de51182fae4255aa19fe1ff098ab2
erzan12 <do.earljan@gmail.com>
2026-05-06 15:32:23 +0800

    feat: added new global interface types

--- apps/web/src/lib/interface/global.interface.ts
+++ apps/web/src/lib/interface/global.interface.ts
@@ -1,4 +1,17 @@
+import { Post, User, Tag } from "@prisma/client";
+
 export interface RecentPostsSidebarProps {
   currentSlug?: string;
   showBackButton?: boolean;
 }
+
+export type PostWithRelations = Post & {
+  author: User;
+  tags: Tag[];
+};
+
+export type BlogClientProps = {
+  posts: PostWithRelations[];
+  avatar: string | undefined;
+  profile: any;
+};
