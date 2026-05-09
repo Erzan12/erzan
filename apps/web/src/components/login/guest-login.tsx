0acdfdb8321e6f691b1a1081096523a84440357e
erzan12 <do.earljan@gmail.com>
2026-05-09 11:54:41 +0800

    feat: now using google and github 0auth for easy verify

--- apps/web/src/components/login/guest-login.tsx
+++ apps/web/src/components/login/guest-login.tsx
@@ -3,6 +3,7 @@
 import { signIn } from "next-auth/react";
 import { Github } from "lucide-react";
 import { useState } from "react";
+import { SiGithub, SiGoogle } from "react-icons/si";
 
 export default function GuestLoginButton() {
   const [name, setName] = useState("");
@@ -18,7 +19,7 @@ export default function GuestLoginButton() {
   
   return (
     <div className="py-10 flex flex-col items-center justify-center gap-4">
-      <input
+      {/* <input
         placeholder="Name"
         onChange={(e) => setName(e.target.value)}
       />
@@ -34,14 +35,24 @@ export default function GuestLoginButton() {
         className="mt-4 inline-flex items-center gap-2 px-15 py-3 bg-slate-400 dark:bg-white text-white dark:text-slate-900 rounded-2xl font-semibold hover:opacity-90 transition-all active:scale-95"
       >
         Login as Guest
-      </button>
+      </button> */}
 
       <button
         onClick={() => signIn("github")} // It will auto-redirect back to the same page after login
         className="mt-2 inline-flex items-center gap-2 px-6 py-3 bg-slate-900 dark:bg-white text-white dark:text-slate-900 rounded-2xl font-semibold hover:opacity-90 transition-all active:scale-95"
       >
-        <Github size={18} />
+        <SiGithub size={18} />
         Continue with GitHub
+      </button>
+
+      <button
+        onClick={() =>
+          signIn("google", { callbackUrl: "/" })
+        }
+        className="mt-2 inline-flex items-center gap-2 px-6 py-3 bg-red-900 dark:bg-white text-white dark:text-slate-900 rounded-2xl font-semibold hover:opacity-90 transition-all active:scale-95"
+      >
+        <SiGoogle size={18} />
+        Continue with Google
       </button>
     </div>
   );
