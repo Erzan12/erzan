96ff173c5b55f10f93e0d438d2e06491c1573f24
erzan <do.earljan@gmail.com>
2026-05-10 21:28:50 +0800

    fix: adjusted client layout causing build errors

--- apps/web/src/components/core/client-layout.tsx
+++ apps/web/src/components/core/client-layout.tsx
@@ -7,9 +7,6 @@ import Providers from "@/components/providers";
 import { usePathname } from "next/navigation";
 import { routeThemes } from "@/lib/constants/themes";
 import { cn } from "@/lib/utils";
-import { Geist } from "next/font/google";
-
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export default function ClientLayout({
   children,
@@ -19,26 +16,20 @@ export default function ClientLayout({
   const pathname = usePathname();
 
   const activeKey =
-    Object.keys(routeThemes).find((route) =>
-      pathname.startsWith(route)
-    ) || "default";
+    Object.keys(routeThemes).find((route) => pathname.startsWith(route)) ||
+    "default";
 
-  const themeClass =
-    routeThemes[activeKey].split(" ")[0];
+  const themeClass = routeThemes[activeKey].split(" ")[0];
 
   return (
-    <html lang="en" className={cn("font-sans", geist.variable)} suppressHydrationWarning>
-       <body className="bg-white dark:bg-black text-black dark:text-white">
-            <div className={cn("transition-colors duration-500 min-h-screen", themeClass)}>
-                <ThemeProvider>
-                    <Providers>
-                    <Navbar />
-                    {children}
-                    <Footer />
-                    </Providers>
-                </ThemeProvider>
-            </div>
-       </body>
-    </html>
+    <div className={cn("transition-colors duration-500 min-h-screen", themeClass)}>
+      <ThemeProvider>
+        <Providers>
+          <Navbar />
+          {children}
+          <Footer />
+        </Providers>
+      </ThemeProvider>
+    </div>
   );
 }
