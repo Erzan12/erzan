8750296769f00971cf2cc015ac592431805ab49b
erzan12 <do.earljan@gmail.com>
2026-05-12 11:29:05 +0800

    feat: added new app router for invitation/token/page

---
+++ apps/web/src/app/admin/my-testimonials/invitations/[token]/page.tsx
@@ -0,0 +1,42 @@
+import { prisma } from "@/lib/prisma/prisma";
+import { redirect } from "next/navigation";
+
+type Props = {
+  params: Promise<{ token: string }>;
+};
+
+export default async function InvitePage({
+  params,
+}: Props) {
+  const { token } = await params;
+
+  const invitation =
+    await prisma.testimonialInvitation.findUnique({
+      where: { token },
+    });
+
+  if (!invitation) {
+    return <div>Invalid invitation</div>;
+  }
+
+  if (invitation.status !== "PENDING") {
+    return <div>Invitation already used</div>;
+  }
+
+  // Expiration check
+  if (new Date() > invitation.expires_at) {
+    await prisma.testimonialInvitation.update({
+      where: { id: invitation.id },
+      data: {
+        status: "EXPIRED",
+      },
+    });
+
+    return <div>Invitation expired</div>;
+  }
+
+  // DO NOT mark accepted here
+  redirect(
+    `/testimonial/create?token=${token}`
+  );
+}
